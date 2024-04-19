f978d41d1e01df73242bd5844352b789df1d84e9
Patrice CAVEZZAN <pcavezzan@gmail.com>
2024-04-19 13:38:18 +0200

    Add chap 15

---
+++ chap-15/src/main.rs
@@ -0,0 +1,15 @@
+fn min<T : Ord>(a: T, b: T) -> T {
+    return a.min(b)
+}
+
+
+fn main() {
+    assert_eq!(min(0, 10), 0);
+    assert_eq!(min(500, 123), 123);
+
+    assert_eq!(min('a', 'z'), 'a');
+    assert_eq!(min('7', '1'), '1');
+
+    assert_eq!(min("hello", "goodbye"), "goodbye");
+    assert_eq!(min("bat", "armadillo"), "armadillo");
+}
